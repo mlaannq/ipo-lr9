c9f03bd34ce55c23223782340d2660ab40edf3e0
mlaannq <naskovecmilana@gmail.com>
2024-12-17 21:39:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from collision.isCorrectRect import isCorrectRect
+'''
+isCorrectRect([(-3.4, 1),(9.2, 10)]) # Вернет True
+
+isCorrectRect([(-7, 9),(3, 6)]) # Вернет False
+'''
 from collision.intersectionAreaRect import intersectionAreaRect
+'''
+intersectionAreaRect([(-3, 1), (9, 10)], [(-7, 0), (13, 12)]) # Вернет некоторое положительное число
+
+intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]) # Вернет 0
+
+intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]) # Вызовет ошибку
+'''
 from collision.isCollisionRect import isCollisionRect
+'''
+isCollisionRect([(-3.4, 1),(9.2, 10)], [(-7.4, 0),(13.2, 12)]) # Вернет True
+
+isCollisionRect([(1, 1),(2, 2)], [(3, 0),(13, 1)]) # Вернет False
+
+isCollisionRect([(1, 1),(2, 2)], [(3, 17),(13, 1)]) # Вызовет ошибку
+'''
 from collision.intersectionAreaMultiRect import intersectionAreaMultiRect
+'''
+rectangles = [
+    [(-3, 1), (9, 10)],
+    [(-7, 0), (13, 12)],
+    [(0, 0), (5, 5)],
+    [(2, 2), (7, 7)]
+]
+incorrect_rectangles = [
+    [(-3, 1), (9, 10)],
+    [(3, 17), (13, 1)]  # Некорректный прямоугольник
+]
+'''
 
 def main():
     while True:
